b91f3c15e8454129f80f5b64068e4eee2aae1cc1
Andrew Dawson <ajdawson@acm.org>
2013-05-10 16:54:10 +0100

    Added compilation step to the test script.

--- runtests.sh
+++ runtests.sh
@@ -3,10 +3,18 @@
 # Run test jobs and report timing information.
 #
 
-TIME="/usr/bin/time -f '%E real, %U user %S sys'"
+# build the executables
+make
+
+# determine the number of processes (MPI workers or OpenMP threads)
+# used for the tests
 NPROCESSES=3
 export OMP_NUM_THREADS=$NPROCESSES
 
+# define the timing command
+TIME="/usr/bin/time -f '%E real, %U user %S sys'"
+
+# evaluate each program in turn
 eval "$TIME ./mm_serial.exe"
 eval "$TIME ./mm_omp.exe"
 eval "$TIME mpirun -n $NPROCESSES mm_mpi.exe"
